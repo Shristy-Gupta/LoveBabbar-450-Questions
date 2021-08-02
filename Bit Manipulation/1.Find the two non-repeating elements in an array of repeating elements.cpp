9c8221df44660e65e5998cd40379ce771d21d5ab
Shristy Gupta <shristysgupta@gmail.com>
2021-08-02 12:20:12 +0530

    Update 1.Find the two non-repeating elements in an array of repeating elements.cpp

--- Bit Manipulation/1.Find the two non-repeating elements in an array of repeating elements.cpp
+++ Bit Manipulation/1.Find the two non-repeating elements in an array of repeating elements.cpp
@@ -8,18 +8,29 @@ public:
     vector<int> singleNumber(vector<int> nums) 
     {
         vector<int> ans;
-        unordered_map<int, int> mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
+        int x=0,y=0;
+        int _xor=nums[0];
+        for(int i=1;i<nums.size();i++){
+            //removes all the repeating numbers
+            _xor^=nums[i];
         }
-        for(int i=0;i<nums.size();i++){
-            if( mp[nums[i]]==1){
-                ans.push_back(nums[i]);
-            }
-        }
-        sort(ans.begin(),ans.end());
-        return ans;
+        int set_bit_rightmost_no= _xor & ~(_xor -1);
         
+        for(int i=0;i<nums.size();i++){
+           if(set_bit_rightmost_no & nums[i]){
+               x^=nums[i];
+           }
+           else{
+               y^=nums[i];
+           }
+        }
+        if(x<y){
+            ans={x,y};
+        }
+        else{
+            ans={y,x};
+        }
+        return ans;
     }
 };
 
