b0b485e064fbcd7688a1a236ec62806f10fbfec1
Shristy Gupta <shristysgupta@gmail.com>
2021-08-02 12:19:27 +0530

    Create 1.Find the two non-repeating elements in an array of repeating elements.cpp

---
+++ Bit Manipulation/1.Find the two non-repeating elements in an array of repeating elements.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> singleNumber(vector<int> nums) 
+    {
+        vector<int> ans;
+        unordered_map<int, int> mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        for(int i=0;i<nums.size();i++){
+            if( mp[nums[i]]==1){
+                ans.push_back(nums[i]);
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+        
+    }
+};
+
+// { Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	vector<int> v(2 * n + 2);
+    	for(int i = 0; i < 2 * n + 2; i++)
+    		cin >> v[i];
+    	Solution ob;
+    	vector<int > ans = ob.singleNumber(v);
+    	for(auto i: ans)
+    		cout << i << " ";
+    	cout << "\n";
+    }
+	return 0;
+}  // } Driver Code Ends
