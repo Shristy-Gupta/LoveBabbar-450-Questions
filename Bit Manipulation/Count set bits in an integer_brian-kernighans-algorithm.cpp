8ecaadb47ee8ae6171ba6f3af1dbc42e16a41e5b
Shristy Gupta <shristysgupta@gmail.com>
2021-08-02 10:13:32 +0530

    Create Count set bits in an integer_brian-kernighans-algorithm.cpp

---
+++ Bit Manipulation/Count set bits in an integer_brian-kernighans-algorithm.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+ 
+// Function to count the total number of set bits in `n`
+int countSetBits(int n)
+{
+    // `count` stores the total bits set in `n`
+    int count = 0;
+ 
+    while (n)
+    {
+        n = n & (n - 1);    // clear the least significant bit set
+        count++;
+    }
+ 
+    return count;
+}
+ 
+int main()
+{
+    int n = -1;
+ 
+    cout << n << " in binary is " << bitset<32>(n) << endl;
+    cout << "The total number of set bits in " << n << " is "
+         << countSetBits(n) << endl;
+ 
+    return 0;
+}
