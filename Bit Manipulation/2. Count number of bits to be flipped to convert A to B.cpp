37f44378ec0fc50915d4a73e6df20ce392589a54
Shristy Gupta <shristysgupta@gmail.com>
2021-08-02 12:37:28 +0530

    Create 2. Count number of bits to be flipped to convert A to B.cpp

---
+++ Bit Manipulation/2. Count number of bits to be flipped to convert A to B.cpp
@@ -0,0 +1,43 @@
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // Function to find number of bits needed to be flipped to convert A to B
+    int countBitsFlip(int a, int b){
+        
+        int c=a ^ b;
+        int count1=0;
+    
+        while(c>0){
+            int rmsb=c & -c;
+            c=c-rmsb;
+            count1++;
+        }
+        return count1;
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver Code
+int main()
+{
+	int t;
+	cin>>t;// input the testcases
+	while(t--) //while testcases exist
+	{
+		int a,b;
+		cin>>a>>b; //input a and b
+
+        Solution ob;
+		cout<<ob.countBitsFlip(a, b)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
