46d49e6397533c59bccc6bcb11eee6f1526f5cc0
Shristy Gupta <shristysgupta@gmail.com>
2021-08-10 15:47:19 +0530

    Create 3 Write a program to Delete loop in a linked list..cpp

---
+++ LinkedList/3 Write a program to Delete loop in a linked list..cpp
@@ -0,0 +1,149 @@
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+bool isLoop(Node* head)
+{
+    if(!head) return false;
+    
+    Node* fast = head->next;
+    Node* slow = head;
+    
+    while( fast != slow)
+    {
+        if( !fast || !fast->next ) return false;
+        fast=fast->next->next;
+        slow=slow->next;
+    }
+    
+    return true;
+}
+
+int length(Node* head)
+{
+    int ret = 0;
+    while(head)
+    {
+        ret++;
+        head = head->next;
+    }
+    return ret;
+}
+
+
+ // } Driver Code Ends
+/*
+structure of linked list node:
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+*/
+#include<unordered_set>
+class Solution
+{
+    public:
+    //Function to remove a loop in the linked list.
+    void removeLoop(Node* head)
+    {
+        unordered_set<Node*> us;
+        Node *ptr=head;
+        Node *link=NULL;
+        while(ptr!=NULL){
+            if(us.find(ptr)!=us.end()){
+                link=ptr;
+                break;
+            }
+            else{
+                us.insert(ptr);
+            }
+            ptr=ptr->next;
+        }
+        if(link==NULL){
+            return;
+        }
+        else{
+            int count=0;
+            while(head!=NULL){
+                if(head->next==link && count==0){
+                    count++;
+                }
+                else if(head->next==link && count!=0){
+                    break;
+                }
+                head=head->next;
+            }
+            head->next=NULL;
+        }
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        Solution ob;
+        ob.removeLoop(head);
+        
+        if( isLoop(head) || length(head)!=n )
+            cout<<"0\n";
+        else
+            cout<<"1\n";
+    }
+	return 0;
+}
+  // } Driver Code Ends
