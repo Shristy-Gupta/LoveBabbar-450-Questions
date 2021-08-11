bcf22b134658e7cd365349be2608cb2559cd0dc8
Shristy Gupta <shristysgupta@gmail.com>
2021-08-11 17:47:12 +0530

    Create 10 Intersection of two Sorted Linked List.cpp

---
+++ LinkedList/10 Intersection of two Sorted Linked List.cpp
@@ -0,0 +1,108 @@
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+    
+    cin>>val;
+    head = tail = new Node(val);
+    
+    while(--size)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+}
+
+Node* findIntersection(Node* head1, Node* head2);
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    cin>> n >> m;
+	    
+	    Node* head1 = inputList(n);
+	    Node* head2 = inputList(m);
+	    
+	    Node* result = findIntersection(head1, head2);
+	    
+	    printList(result);
+	    cout<< endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+/* The structure of the Linked list Node is as follows:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+Node* findIntersection(Node* head1, Node* head2)
+{
+    Node* head=NULL;
+    Node* tail=NULL;
+    while(head1!=NULL && head2!=NULL){
+        if(head1->data==head2->data){
+            if(head==NULL){
+                head=new Node(head1->data);
+                tail=head;
+            }
+            else{
+                tail->next=new Node(head1->data);
+                tail=tail->next;
+            }
+            head1=head1->next;
+            head2=head2->next;
+        }
+        else if(head1->data<head2->data){
+            head1=head1->next;
+        }
+        else{
+            head2=head2->next;
+        }
+    }
+    return head;
+}
