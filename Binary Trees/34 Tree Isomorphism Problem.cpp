238d8355ab6c1fdbd16016a090ace1369a4e3c13
Shristy Gupta <shristysgupta@gmail.com>
2021-09-18 00:49:19 +0530

    Create 34 Tree Isomorphism Problem.cpp

---
+++ Binary Trees/34 Tree Isomorphism Problem.cpp
@@ -0,0 +1,18 @@
+class Solution{
+  public:
+    // Return True if the given trees are isomotphic. Else return False.
+    bool isIsomorphic(Node *root1,Node *root2)
+    {
+       if(root1==NULL && root2==NULL){
+           return true;
+       }
+       if((root1==NULL && root2!=NULL) || (root1!=NULL && root2==NULL)){
+           return false;
+       }
+       if(root1->data!=root2->data){
+           return false;
+       }
+       return isIsomorphic(root1->right,root2->left) && isIsomorphic(root1->left,root2->right)
+       || isIsomorphic(root1->left,root2->left) && isIsomorphic(root1->right,root2->right);
+    }
+};
